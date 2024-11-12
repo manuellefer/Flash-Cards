82de4852f3b4a64bd5dc5b718396b3e3eaea8687
manu <138732305+manuellefer@users.noreply.github.com>
2024-11-12 14:12:49 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao (
+    'Programação',
+    'Para que serve o main de uma pagina html?',
+    'Serve para eu anexar todo conteúdo principal do meu site'
+)
+
+criaCartao (
+    'Robótica',
+    'Por que o carrinho de robótica não deu certo?',
+    'Estava com mal contato no jumper que conectava na placa'
+)
+
+criaCartao (
+    'História',
+    'Quais foram algumas das principais revoltas que ocorreram durante a Primeira República do Brasil?',
+    'Revolta das vacinas, revolta de canudos, revolução federalista, movimento do contestado.'
+)
+
+criaCartao (
+    'Geografia',
+    'Quais são as estações do ano ?',
+    'Outono, Inverno, Verão, Primavera'
+)
