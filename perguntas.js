8493a9d37de0ac148d5f1f9ac22426531fd38688
manu <138732305+manuellefer@users.noreply.github.com>
2024-11-13 00:36:38 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,87 @@ criaCartao (
     'Quais são as estações do ano ?',
     'Outono, Inverno, Verão, Primavera'
 )
+
+criaCartao (
+    'Geografia',
+    'O que é neoliberalismo?',
+    'É uma corrente econômica que defende a liberdade de mercado, a desregulamentação e a redução do papel do Estado na economia.'
+)
+
+criaCartao (
+    'Geografia',
+    'O que é o desemprego estrutural?',
+    'Refere-se a mudanças de longo prazo na economia que afetam a demanda para certos tipos de trabalho.'
+)
+
+criaCartao (
+    'Geografia',
+    'O que é o desemprego conjuntural?',
+    'Resulta de flutuações de curto prazo no ciclo econômico.'
+)
+
+criaCartao (
+    'Geografia',
+    'Quais são as responsabilidades da ONU?',
+    'Manter a paz e a segurança internacional, desenvolver relações amistosas entre as nações,entre outros.'
+)
+
+criaCartao (
+    'Geografia',
+    'Quais os países que formam o BRICS?',
+    'Brasil, Rússia, Índia, China e África do Sul.'
+)
+
+criaCartao (
+    'Geografia',
+    'O que é um produto manufaturado?',
+    'São aqueles que são fabricados em grande quantidade, de forma padronizada e em série.'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual país que mais importa produtos brasileiros?',
+    'O chile.'
+)
+
+criaCartao (
+    'Geografia',
+    'O que é gentrificação?',
+    'É processo que ocorre em áreas urbanas quando em uma área que antes era considerada degradada ou carente começa a atrair investimentos e novos moradores com poder aquisitivo maior.'
+)
+
+criaCartao (
+    'Geografia',
+    'O que é especulação imobiliária?',
+    'É a prática de pessoas de comprar imóveis esperando que seus preços aumentem no futuro para vendê-los com lucro.'
+)
+
+criaCartao (
+    'Geografia',
+    'Quais os problemas enfrentados nas fronteiras brasileiras?',
+    'Tráfico de drogas, contrabando, imigração ilegal.'
+)
+
+criaCartao (
+    'Geografia',
+    'Quem faz o controle das fronteiras brasileiras?',
+    'A Polícia Federal, Receita Federal, Forças Armadas e agentes da Vigilância Sanitária.'
+)
+
+criaCartao (
+    'Geografia',
+    'O que é contrabando?',
+    'São importações e exportações de produtos proibidos no país.'
+)
+
+criaCartao (
+    'Geografia',
+    'O que é descaminho?',
+    'São produtos comercializados sem o recolhimento do imposto.'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual é o órgão no Brasil responsável pela demarcação de terras índigenas no Brasil?',
+    'A FUNAI (Fundação Nacional dos Povos Indígenas).'
+)
